b6f6f01227db35e03ef2ae5349d6c98aca1d2270
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2021-03-14 21:30:16 +0100

    Ajout de UserDataPersister pour ajouter un user

---
+++ src/DataPersister/UserDataPersister.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataPersister;
+
+use App\Entity\User;
+use App\Service\UserService;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+
+final class UserDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User;
+    }
+
+    public function persist($data, array $context = [])
+    {
+        $user = $this->userService->persist($data);
+        return $user;
+    }
+
+    public function remove($data, array $context = [])
+    {
+        // call your persistence layer to delete $data
+    }
+}
---
+++ src/Service/UserService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserService
+{
+    private $manager;
+    private $repository;
+    private $encoder;
+
+    public function __construct(EntityManagerInterface $manager, UserRepository $repository, UserPasswordEncoderInterface $encoder)
+    {
+        $this->manager = $manager;
+        $this->repository = $repository;
+        $this->encoder = $encoder;
+    }
+    
+    /**
+     * @param  User $user
+     * @return User
+     */
+    public function persist(User $user): User
+    {
+        $user->setPassword($this->encoder->encodePassword($user, $user->getPassword()));
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        return $user;
+    }
+}
